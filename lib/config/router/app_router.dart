815cbb03fdc7499cf1b4c14d84a412306fd0f298
Marcelo-012 <marcelorivero9419@gmail.com>
2026-06-28 02:26:37 -0600

    creacion de rutas, homeScreen,

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,16 @@
+import '/presentation/screens/screens.dart';
+
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    //
+    GoRoute(
+      //
+      path: '/',
+      name: 'home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
